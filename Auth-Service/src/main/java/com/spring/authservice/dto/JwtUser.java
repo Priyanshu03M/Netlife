e6f02fd563b2c7958c12fce8cbb8728ce58066dd
Priyanshu03M <97392355+Priyanshu03M@users.noreply.github.com>
2026-03-02 19:01:20 +0530

    Added Validation

--- Auth-Service/src/main/java/com/spring/authservice/dto/JwtUser.java
+++ Auth-Service/src/main/java/com/spring/authservice/dto/JwtUser.java
@@ -1,8 +1,15 @@
 package com.spring.authservice.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
 
-public record JwtUser(String username, Collection<? extends GrantedAuthority> authorities) {
+public record JwtUser(
+        @NotBlank(message = "Username is required") String username,
+        @NotNull(message = "Authorities are required")
+        @NotEmpty(message = "Authorities must not be empty") Collection<? extends GrantedAuthority> authorities
+) {
 }
