3abeb2c19342996ffc41c418193663c7f66bda63
Priyanshu03M <97392355+Priyanshu03M@users.noreply.github.com>
2026-03-15 23:10:18 +0530

    Integrated RabbitMQ

---
+++ Auth-Service/src/main/java/com/spring/authservice/dto/AccountMsgDto.java
@@ -0,0 +1,14 @@
+package com.spring.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccountMsgDto {
+    private String name;
+}
