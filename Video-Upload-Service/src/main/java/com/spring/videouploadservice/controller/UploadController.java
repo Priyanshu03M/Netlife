ddc553ffa16ff19892d728c45139cbd0a21db3b7
Priyanshu03M <97392355+Priyanshu03M@users.noreply.github.com>
2026-03-19 22:45:29 +0530

    Refactor

--- Video-Upload-Service/src/main/java/com/spring/videouploadservice/controller/UploadController.java
+++ Video-Upload-Service/src/main/java/com/spring/videouploadservice/controller/UploadController.java
@@ -6,18 +6,16 @@ import com.spring.videouploadservice.service.UploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/videos")
 public class UploadController {
     private final UploadService uploadService;
 
-    @PostMapping("/videos/upload")
+    @PostMapping("/upload")
     public ResponseEntity<?> upload(
             @RequestParam("file") MultipartFile file,
             @RequestParam("title") String title,
